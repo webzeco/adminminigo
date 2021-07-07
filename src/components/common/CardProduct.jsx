00020c7e294d45d3cf699b4a1218be07eda90fdf
Ali Qadeer <aliqadeer9233@gmanil.com>
2021-07-08 00:47:42 +0500

    design show product card

--- src/components/common/CardProduct.jsx
+++ src/components/common/CardProduct.jsx
@@ -1,59 +1,78 @@
 import React from "react";
+import "../styles/card.css";
 const CardProduct = ({product}) => {
   return (
     <>
-      <div className="container card">
-        <div className="row text-center d-flex justify-content-around bg-light pt-4 transCard">
-          <div className="col-sm-6 col-md-4 col-lg-2">
-            <div className="pad">
+      <div className="container card mx-3 w-auto ">
+        <div className="row text-center d-flex justify-content-around bg-light pt-3 transCard">
+          <div className="col-sm-4 col-md-2 col-lg-2  ">
+            <div className="pad text-center">
               <img
-                className="img"
-                src={`https://arcane-meadow-07029.herokuapp.com/img/users/${product.images[0]}`}
+                className="img-fluid w-50"
+                src={"https://www.freeiconspng.com/uploads/women-bag-png-32.png"} 
                 alt="Sender"
               />
-              <p>{product.title}</p>
+             
             </div>
           </div>
-          <div className="col-sm-6 col-md-4 col-lg-2 ">
+          <div className="col-sm-4 col-md-2 col-lg-2 mt-3">
             <div className="pad">
-              <h5>Price</h5>
+            <h4 className="fw-bold">{product.title}</h4>
+            </div>
+          </div>
+
+          <div className="col-sm-4 col-md-2 col-lg-2 mt-3">
+            <div className="pad">
+            
               <p>
                 {product.price}
                 <sub>Rs</sub>
               </p>
             </div>
           </div>
-          <div className="col-sm-6  col-md-4  col-lg-2">
+
+          <div className=" col-sm-4  col-md-2 col-lg-2 mt-3">
             <div className="pad">
-              <h5>Date</h5>
+            
+              <p className="status_look pt-1 pb-1">Active</p>
+            </div>
+          </div>
+
+
+          <div className=" col-sm-4  col-md-2  col-lg-2 mt-3">
+            <div className="pad">
+            
               <p>
                 {new Date(product.createdAt).toLocaleDateString()}
               </p>
             </div>
           </div>
-          <div className="col-sm-6 col-md-4 col-lg-2 ">
+       
+       
+
+          <div className=" col-sm-4  col-md-2 col-lg-2">
             <div className="pad">
-              <h5>Time</h5>
-              <p>
-                {new Date(product.createdAt).toLocaleTimeString()}
-              </p>
-            </div>
-          </div>
-          <div className="col-sm-6  col-md-4 col-lg-2 ">
-            <div className="pad">
-              <h5>Type</h5>
-              <p>{product.method}</p>
-            </div>
-          </div>
-          <div className="col-sm-6  col-md-4 col-lg-2">
-            <div className="pad">
-              <button
+              <div className="row">
+                <div className="col-12">
+            <button
                 type="button"
-                className="btn btn-primary btn-lg btn-block "
+                className="btn btn-success   btn-block "
                 // onClick={}
               >
-                Confirm
+                Edit
               </button>
+              </div>
+              <div className="col-12">
+              <button
+                type="button"
+                className="btn btn-white btn-block "
+                // onClick={}
+              >
+             
+                Delete
+              </button>
+              </div>
+              </div>
             </div>
           </div>
         </div>
